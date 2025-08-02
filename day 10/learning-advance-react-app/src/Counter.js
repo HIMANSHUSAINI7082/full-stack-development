9ce4dc7141e1c6afab95df41b513f32f6278a3ac
himanshu saini <haryanvicircle1234@gmail.com>
2025-08-02 15:52:33 +0530

    day 10 work is done

---
+++ day 10/learning-advance-react-app/src/Counter.js
@@ -0,0 +1,19 @@
+import { useState} from 'react'
+
+function Counter() {
+    // here count is a simple variablevwhich does not provide memory 
+    // let count = 0;
+
+    //count is a state which provide  memory and function setcount to update count state
+    // let [mystate ,setMystate] = usestate(default value)
+    let [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={() => setCount(count + 1)}>click me</button>
+        </div>
+    )
+}
+
+export {Counter}
